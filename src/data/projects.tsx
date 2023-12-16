32e37fd5a8e17e50717f64ca8576883f9c9f690f
jed-3rd <jed.shppng@gmail.com>
2023-12-16 02:58:43 -0700

    Update Projects

--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -23,11 +23,7 @@ export const projects: ProjectType[] = [
         tech: [
             "GoLang",
             "Google Cloud",
-            "Python",
-            "AWS",
-            "MongoDB",
-            "SQL",
-            "Pandas & Numpy"
+            "FireStore"
         ]
     },
     {
